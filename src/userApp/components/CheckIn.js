3ee9d9abae60cdb0e140ca81269c0364048296bb
AlexandraMnc <manciualexandra15@gmail.com>
2021-01-19 20:06:15 +0000

    Check in functionality done - no styling

--- src/userApp/components/CheckIn.js
+++ src/userApp/components/CheckIn.js
@@ -5,6 +5,7 @@ import { placeAbi} from '../abi';
 import 'react-native-get-random-values';
 import { styles } from '../appStyles.js';
 import { View, Text } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 import { ethers } from 'ethers';
 
@@ -13,6 +14,7 @@ export default function CheckIn() {
     //hooks
     const [isOverlayVisible, setOverlayVisible] = useState(false);
     const [walletAddress, setWalletAddress] = useState(0);
+    const [buttonState, setButtonState] = useState('NotPressed');
 
     /* Open confirmation modal */
     const toggleOverlay = () => {
@@ -32,19 +34,33 @@ export default function CheckIn() {
 
    /* Check in to a place and generate unique key for the place validation.*/
    const checkIn = () => {}
-  
-   return (
-        <View>
+   
+   if(isOverlayVisible == false){
+        return (
             <Button
                 onPress={createWallet}
                 title="Check in"
             />
-            <Overlay isVisible={isOverlayVisible}>
-                <View>
-                    <Text>Your address is: {walletAddress}</Text>
-                    <Button title="Done" onPress={toggleOverlay} />
-                </View>
-            </Overlay>
-        </View>
-   );
+        );
+   }
+   if(isOverlayVisible == true){
+        return(
+            <View>
+                <Text>
+                   Show this QR code to complete the check in
+                </Text>
+                <QRCode 
+                    value={walletAddress}
+                    size={200}
+                    bgColor='black'
+                    fgColor='white'
+                />
+                <Button
+                    onPress={toggleOverlay}
+                    title="Done"
+                />
+            </View>
+        );
+   }
+
 }
