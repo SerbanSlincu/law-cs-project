02c676181566d6689d82d154ab9e81ca2e5bedf3
AlexandraMnc <manciualexandra15@gmail.com>
2021-01-19 12:27:43 +0000

    Added method for creating a new wallet

--- src/userApp/App.js
+++ src/userApp/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'react-native-elements';
 import { placeAbi} from './abi';
+import 'react-native-get-random-values';
 
 //const Web3 = require('web3');
 //const web3 = new Web3('http://localhost:8545');
@@ -10,7 +11,7 @@ import { ethers } from 'ethers';
 
 export default function App() {
 
-    /* Web3 test 
+    /* Web3 test - not working 
     const testPlace = () => {
     
         var contractAddress = '0xC7C01DC9bA41593CE50679D4B58aD14F258B72e5'
@@ -18,18 +19,25 @@ export default function App() {
         console.log(contract)
     } */
 
-    /* Ethers test */
+    /* (Working) Usage of accessing a deployed contract */
     const testPlace = () => {
         let provider = ethers.getDefaultProvider('ropsten');
+        // Address of a deployed Place contract
         var contractAddress = '0x5646c8280a27a5cA4Fb320b06905B6fdc329b432'
         var contract = new ethers.Contract(contractAddress, placeAbi, provider);
         console.log(contract)
    }
+   
+    /* (Working) Usage of creating a new wallet */
+    const createWallet = () => {
+        var wallet = new ethers.Wallet.createRandom();
+        console.log(wallet)
+   }
 
   return (
         <Button
-            onPress={testPlace}
-            title="Access place"
+            onPress={createWallet}
+            title="Create new account"
         />
   );
 }
